0b3d43d6cf081ae42fadd3c102ac887813ad54b2
kobala58 <34425167+kobala58@users.noreply.github.com>
2018-11-23 23:48:29 +0100

    Wyswietlanie pojednynczych slowek dziala.

--- quiz.py
+++ quiz.py
@@ -7,7 +7,7 @@ from flaskext.mysql import MySQL
 app = Flask(__name__)
 
 mysql = MySQL()
-# MySQL configurations
+# MySQL configurations########################
 app.config['MYSQL_DATABASE_USER'] = 'root'
 app.config['MYSQL_DATABASE_PASSWORD'] = ''
 app.config['MYSQL_DATABASE_DB'] = 'slownictwo'
@@ -15,17 +15,30 @@ app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 mysql.init_app(app)
 conn = mysql.connect()
 cursor = conn.cursor()
+###############################################
 
-@app.route('/dzialy')
+@app.route('/dzialy',methods=['POST', 'GET'])
 def dzialy():
+
+    if request.method=='POST':
+       a=request.form['dzial']
+       b=request.form['wtg']
+       b = int(b)
+       cursor.execute('SELECT * FROM {0} ORDER BY RAND() LIMIT {1}'.format(a,b))
+       slowka = cursor.fetchall()
+       return  render_template('dzialy.html',slowka = slowka)
+
+
+@app.route('/' , methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        print("work")
+        return  render_template('dzialy.html')
+
     cursor.execute('SHOW TABLES')
     dzialy = cursor.fetchall()
-    return  render_template('dzialy.html',dzialy=dzialy)
+    return  render_template('index.html',dzialy=dzialy)
 
-@app.route('/')
-def index():
-    # return 'Cześć, tu Python!'
-    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
